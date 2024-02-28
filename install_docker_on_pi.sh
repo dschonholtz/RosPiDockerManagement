7006d6f28ab0ca6fa92bfb21556e3a1eb5958138
Douglas Schonholtz <schonholtzd@gmail.com>
2024-02-28 17:14:54 -0500

    docker for ubuntu bb

--- install_docker_on_pi.sh
+++ install_docker_on_pi.sh
@@ -7,16 +7,16 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates software-properties-common
 
 # Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/raspberry_pi/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add the Docker repository to APT sources
-echo "deb [arch=armhf] https://download.docker.com/linux/raspberry_pi $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
 # Update the package database with Docker packages from the newly added repo
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Add current user to the Docker group to run Docker commands without sudo
 sudo usermod -aG docker $USER
@@ -30,4 +30,4 @@ sudo systemctl enable docker
 # Start Docker service
 sudo systemctl start docker
 
-echo "Docker installation on Raspberry Pi 5 is complete."
+echo "Docker installation on Ubuntu 23.04 for Raspberry Pi is complete."
