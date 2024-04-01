d475c7c8a0a2559bb43576cd40ba08f5f309dd82
Douglas Schonholtz <schonholtzd@gmail.com>
2024-04-01 17:09:01 -0400

    build the packages plz

--- scripts_volume/publish.sh
+++ scripts_volume/publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Source the ROS setup script
 source /opt/ros/noetic/setup.bash
+cd /home/ubuntu/catkin_ws/
+catkin build
 
 # Run your ROS launch file
 roslaunch /home/ubuntu/catkin_ws/src/capstone_robot/launch/capstone_mast_rpi.launch
