3ef102c7ff5c177747a82f56cfb84b17ef4da57a
Douglas Schonholtz <schonholtzd@gmail.com>
2024-04-01 17:26:57 -0400

    push to main lol

--- scripts_volume/publish.sh
+++ scripts_volume/publish.sh
@@ -4,5 +4,7 @@ source /opt/ros/noetic/setup.bash
 cd /home/ubuntu/catkin_ws/
 catkin build
 
+source /home/ubuntu/catkin_ws/devel/setup.bash
 # Run your ROS launch file
 roslaunch /home/ubuntu/catkin_ws/src/capstone_robot/launch/capstone_mast_rpi.launch
+
